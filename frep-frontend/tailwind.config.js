8e4779998356ecbb7a89f62eb7468e70857a856f
good12343 <abrehymalqedy66@gmail.com>
2025-08-29 16:27:41 +0000

    important

---
+++ frep-frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+      "./index.html",
+          "./src/**/*.{js,ts,jsx,tsx}", // يخلي Tailwind يقرأ كل ملفاتك داخل src
+            ],
+              theme: {
+                  extend: {},
+                    },
+                      plugins: [],
+                      } 
