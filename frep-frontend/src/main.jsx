739882aa074b98a85ce0157c475adda649ba5eb4
good12343 <abrehymalqedy66@gmail.com>
2025-08-29 17:13:17 +0000

    yes

--- frep-frontend/src/main.jsx
+++ frep-frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
+import App from '../App';
+import '../index.css';
 
 // Solana wallet adapter
 import { clusterApiUrl } from '@solana/web3.js';
