d18144b422d2067a495ad3e0adbebe8b59419acd
good12343 <abrehymalqedy66@gmail.com>
2025-08-30 14:57:56 +0000

    iY

--- frep-frontend/postcss.config.js
+++ frep-frontend/postcss.config.js
@@ -2,6 +2,10 @@
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
-export default {
-  plugins: [tailwindcss, autoprefixer],
-  };
+  module.exports = {
+      plugins: {
+          '@tailwindcss/postcss': {},
+              autoprefixer: {},
+                },
+                };
+  
